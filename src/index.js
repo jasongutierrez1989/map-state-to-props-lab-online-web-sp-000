33f24b4dc32bd49d2a419405d84deb68fb82efe1
JASON GUTIERREZ <jasongutierrez1989@gmail.com>
2020-08-25 23:28:45 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -2,14 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import shoppingListItemReducer from './reducers/shoppingListItemReducer';
 import App from './App'
 
 import manageUsers from './reducers/manageUsers'
 
 
 const store = createStore(
-  shoppingListItemReducer,
+  manageUsers,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
